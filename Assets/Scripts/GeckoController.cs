10a6fe44f44338b50fd9201c741b0c4ddaa6261a
Alex Popov <popov.alex.elf@gmail.com>
2019-09-20 09:18:33 +0300

    neck movement

---
+++ Assets/Scripts/GeckoController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeckoController : MonoBehaviour
+{
+	[SerializeField] private Transform target;
+	[SerializeField] private Transform headBone;
+
+	private void LateUpdate()
+	{
+		Vector3 towardObjectFromHead = target.position - headBone.position;
+		headBone.rotation = Quaternion.LookRotation(towardObjectFromHead, transform.up);
+	}
+}

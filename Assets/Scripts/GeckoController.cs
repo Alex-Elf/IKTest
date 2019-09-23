b62c4fea1752cd83ce9aeec8b466a5b2e26c6e5f
Alex Popov <popov.alex.elf@gmail.com>
2019-09-23 21:06:04 +0300

    leg stepping done

--- Assets/Scripts/GeckoController.cs
+++ Assets/Scripts/GeckoController.cs
@@ -1,15 +1,128 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+#pragma warning disable 0649
 public class GeckoController : MonoBehaviour
 {
 	[SerializeField] private Transform target;
+
+	[Header("Head tracking")]
 	[SerializeField] private Transform headBone;
+	[SerializeField] private float headTurningSpeed = 20;
+	[SerializeField] private float headMaxTurnAngle = 45;
+	[Header("Eye tracking")]
+	[SerializeField] private Transform leftEye;
+	[SerializeField] private Transform rightEye;
+	[SerializeField] private float eyeMinYRotation;
+	[SerializeField] private float eyeMaxYRotation;
+	[SerializeField] private float eyeTurningSpeed = 20;
+	[Header("Legs")]
+	[SerializeField] private LegStepper LBStepper;
+	[SerializeField] private LegStepper RBStepper;
+	[SerializeField] private LegStepper LFStepper;
+	[SerializeField] private LegStepper RFStepper;
+
+	private void Awake()
+	{
+		StartCoroutine(LegUpdateCoroutine());
+	}
 
 	private void LateUpdate()
 	{
-		Vector3 towardObjectFromHead = target.position - headBone.position;
-		headBone.rotation = Quaternion.LookRotation(towardObjectFromHead, transform.up);
+		HeadTracking();
+		EyeTracking();
+	}
+
+	private void HeadTracking()
+	{
+		Quaternion currLocalRotation = headBone.localRotation;
+		headBone.localRotation = Quaternion.identity;
+
+
+		Vector3 targetWorldLookDir = target.position - headBone.position;
+		Vector3 targetLocalLookDir = headBone.InverseTransformDirection(targetWorldLookDir);
+
+		targetLocalLookDir = Vector3.RotateTowards(
+			Vector3.forward,
+			targetLocalLookDir,
+			Mathf.Deg2Rad * headMaxTurnAngle,
+			0);
+
+
+		Quaternion targetLocalRotation = Quaternion.LookRotation(targetLocalLookDir, Vector3.up);
+
+		headBone.localRotation = Quaternion.Slerp(
+			currLocalRotation,
+			targetLocalRotation,
+			1 - Mathf.Exp(-headTurningSpeed * Time.deltaTime)
+		);
+	}
+	private void EyeTracking()
+	{
+		Quaternion targetEyeRotation = Quaternion.LookRotation(
+			target.position - headBone.position,
+			Vector3.up);
+		leftEye.rotation = Quaternion.Slerp(
+			leftEye.rotation,
+			targetEyeRotation,
+			1 - Mathf.Exp(-eyeTurningSpeed * Time.deltaTime));
+		rightEye.rotation = Quaternion.Slerp(
+			rightEye.rotation,
+			targetEyeRotation,
+			1 - Mathf.Exp(-eyeTurningSpeed * Time.deltaTime));
+
+		float rightEyeYRotationCurrent = rightEye.localEulerAngles.y;
+		float leftEyeYRotationCurrent = leftEye.localEulerAngles.y;
+
+		// Move the rotation to a -180 ~ 180 range
+		if (leftEyeYRotationCurrent > 180)
+		{
+			leftEyeYRotationCurrent -= 360;
+		}
+		if (rightEyeYRotationCurrent > 180)
+		{
+			rightEyeYRotationCurrent -= 360;
+		}
+
+		float rightEyeYRotationClamped = Mathf.Clamp(
+			rightEyeYRotationCurrent,
+			eyeMinYRotation,
+			eyeMaxYRotation);
+		float leftEyeYRotationClamped = Mathf.Clamp(
+			leftEyeYRotationCurrent,
+			eyeMinYRotation,
+			eyeMaxYRotation);
+
+		leftEye.localEulerAngles = new Vector3(
+				leftEye.localEulerAngles.x,
+				leftEyeYRotationClamped,
+				0);
+		rightEye.localEulerAngles = new Vector3(
+			rightEye.localEulerAngles.x,
+			rightEyeYRotationClamped,
+			0
+		);
+		
+	}
+	private IEnumerator LegUpdateCoroutine()
+	{
+		while (true)
+		{
+			do
+			{
+				RBStepper.TryMove();
+				LFStepper.TryMove();
+				yield return null;
+
+			} while (RBStepper.moving || LFStepper.moving);
+
+			do
+			{
+				LBStepper.TryMove();
+				RFStepper.TryMove();
+				yield return null;
+
+			} while (LBStepper.moving || RFStepper.moving);
+		}
 	}
 }
---
+++ Assets/Scripts/LegStepper.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#pragma warning disable 0649
+public class LegStepper : MonoBehaviour
+{
+	[SerializeField] private Transform homeTransform;
+	[SerializeField] private float stepDistance;
+	[SerializeField] private float stepDuration;
+	/// <summary>
+	/// Fraction of the max distance from home we want to overshoot by
+	/// </summary>
+	[SerializeField] float stepOvershootFraction;
+
+
+	public bool moving;
+
+	public void TryMove()
+	{
+		if (moving) return;
+
+		float distFromHome = Vector3.SqrMagnitude(transform.position - homeTransform.position);
+
+		if (distFromHome > stepDistance)
+		{
+			StartCoroutine(MoveToHome());
+		}
+
+	}
+
+
+	private IEnumerator MoveToHome()
+	{
+		moving = true;
+
+		Quaternion startRot = transform.rotation;
+		Vector3 startPos = transform.position;
+
+		Quaternion endRot = homeTransform.rotation;
+
+		Vector3 towardHome = (homeTransform.position - transform.position);
+		float overshootDistance = stepDistance * stepOvershootFraction;
+		Vector3 overshootVector = towardHome * overshootDistance;
+		overshootVector = Vector3.ProjectOnPlane(overshootVector, Vector3.up);
+
+		Vector3 endPos = homeTransform.position + overshootVector;
+
+		Vector3 centerPoint = (startPos + endPos) / 2;
+		centerPoint += homeTransform.up * Vector3.Distance(startPos, endPos) / 2f;
+
+		float timeElapsed = 0;
+
+		do
+		{
+			timeElapsed += Time.deltaTime;
+
+			float normalizedTime = timeElapsed / stepDuration;
+			normalizedTime = Easing.Cubic.InOut(normalizedTime);
+
+			transform.position = Vector3.Lerp(
+				Vector3.Lerp(startPos, centerPoint, normalizedTime),
+				Vector3.Lerp(centerPoint, endPos, normalizedTime),
+				normalizedTime);
+
+
+			transform.rotation = Quaternion.Slerp(startRot, endRot, normalizedTime);
+
+			yield return null;
+		}
+		while (timeElapsed < stepDuration);
+
+		moving = false;
+	}
+}
